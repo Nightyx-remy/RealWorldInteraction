bcecdc10dbc7e0912390ed531a507f6bc0da5176
remy <remy.forjanic@gmail.com>
2022-06-18 15:43:15 +0200

    Push

--- Avalonia.NETCoreMVVMApp1/Views/MainWindow.axaml.cs
+++ Avalonia.NETCoreMVVMApp1/Views/MainWindow.axaml.cs
@@ -1,8 +1,10 @@
+using System;
 using Avalonia.Controls;
 
 namespace Avalonia.NETCoreMVVMApp1.Views {
     public partial class MainWindow : Window {
         public MainWindow() {
+            Console.Out.WriteLine("Hello");
             InitializeComponent();
         }
     }
